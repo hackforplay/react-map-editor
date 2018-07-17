391ba67920c3acd1b471b48260d3c57d0f953f0a
teramotodaiki <i101323@gmail.com>
2018-07-17 17:28:49 +0900

    feature: webpack.config.js のフォーマット, example の修正, mode の追加

--- webpack.config.js
+++ webpack.config.js
@@ -1,34 +1,39 @@
 const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: {
-        'react-map-editor': './src/index.tsx',
-        'example': './src/example.tsx'
-    } ,
-    output: {
-        path: path.resolve(__dirname, 'umd'),
-        filename: '[name].js',
-        library: 'ReactMapEditor',
-        libraryTarget: 'umd'
-    },
-    module: {
-        rules: [{
-            test: /\.(j|t)sx?$/,
-            exclude: /node_modules/,
-            use: {
-                loader: 'babel-loader'
-            }
-        }]
-    },
-    plugins: [
-        new HtmlWebpackPlugin()
-      ],
-    resolve: {
-        extensions: ['.ts', '.tsx', '.js', '.json', '.jsx'],
-    },
-    devtool: 'source-map',
-    devServer: {
-        contentBase: path.resolve(__dirname, 'public'),
-    }
-}
+  mode: process.env.NODE_ENV || 'development',
+  entry: {
+    'react-map-editor': './src/index.tsx',
+    example: './src/example.tsx'
+  },
+  output: {
+    path: path.resolve(__dirname, 'umd'),
+    filename: '[name].js',
+    library: 'ReactMapEditor',
+    libraryTarget: 'umd'
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(j|t)sx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      chunks: ['example']
+    })
+  ],
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.json', '.jsx']
+  },
+  devtool: 'source-map',
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public')
+  }
+};
