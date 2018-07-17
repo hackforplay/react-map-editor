6e2d6b400e870bccc0e1e1b433a30b9b4215a6b3
teramotodaiki <i101323@gmail.com>
2018-07-17 18:11:04 +0900

    feature: example の root element を最大化

--- src/example.tsx
+++ src/example.tsx
@@ -1,8 +1,15 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
+import { normalize, setupPage } from 'csstips';
 import RootComponent from '.';
 
+const rootId = 'root';
+
 const container = document.createElement('div');
+container.id = rootId;
 document.body.appendChild(container);
+
+normalize();
+setupPage(`#${rootId}`);
 
 ReactDOM.render(<RootComponent />, container);
