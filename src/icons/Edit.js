ecac1dc6d82b7d8cdf97b3f3796493e9cbdd28ad
teramotodaiki <i101323@gmail.com>
2018-07-19 17:08:41 +0900

    feature: svg アイコンの追加と svgr で ReactComponent に変換

---
+++ src/icons/Edit.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Edit = props => (
+  <svg viewBox="0 0 18 18" width="1em" height="1em" {...props}>
+    <g fill="none" fillRule="evenodd">
+      <path
+        d="M0 14.25V18h3.75L14.81 6.94l-3.75-3.75L0 14.25zM17.71 4.04a.996.996 0 0 0 0-1.41L15.37.29a.996.996 0 0 0-1.41 0l-1.83 1.83 3.75 3.75 1.83-1.83z"
+        fill="currentColor"
+        fillRule="nonzero"
+      />
+      <path d="M-3-3h24v24H-3z" />
+    </g>
+  </svg>
+);
+
+export default Edit;
---
+++ src/icons/Eraser.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const Eraser = props => (
+  <svg viewBox="0 0 18 18" width="1em" height="1em" {...props}>
+    <g fill="none" fillRule="evenodd">
+      <path
+        d="M18 3.75V0h-3.75L3.19 11.06l3.75 3.75L18 3.75zM.29 13.96a.996.996 0 0 0 0 1.41l2.34 2.34c.39.39 1.02.39 1.41 0l1.83-1.83-3.75-3.75-1.83 1.83z"
+        fill="currentColor"
+        fillRule="nonzero"
+      />
+      <path d="M21 21H-3V-3h24z" />
+    </g>
+  </svg>
+);
+
+export default Eraser;
