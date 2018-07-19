4c38a179585550fce1a3abdb205a10d3044738aa
teramotodaiki <i101323@gmail.com>
2018-07-19 15:11:07 +0900

    feature: いくつかの ActionCreator をロギングしないように

--- src/redux/createStore.ts
+++ src/redux/createStore.ts
@@ -1,7 +1,7 @@
 import * as redux from 'redux';
 import { Action } from 'typescript-fsa';
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
-import logger from 'redux-logger';
+import { createLogger } from 'redux-logger';
 import { palette, canvas, input, Store } from '.';
 
 export const rootEpic = combineEpics(palette.epics, canvas.epics, input.epics);
@@ -17,6 +17,16 @@ const epicMiddleware = createEpicMiddleware<Action<any>, any, Store>();
 export default function createStore() {
   const composeEnhancers =
     (window as any).__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || redux.compose;
+
+  const ignoreLogger = [
+    input.actions.mouseMove,
+    input.actions.mouseEnter,
+    input.actions.mouseLeave
+  ];
+
+  const logger = createLogger({
+    predicate: (getState, action) => !ignoreLogger.some(ac => ac.match(action))
+  });
 
   const store = redux.createStore(
     rootReducer,
