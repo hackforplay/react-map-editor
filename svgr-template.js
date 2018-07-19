e509b84d1583ca14ff739ff59fc0ebee61becbea
teramotodaiki <i101323@gmail.com>
2018-07-19 22:05:08 +0900

    feature: svg component にアノテーションを追加

--- svgr-template.js
+++ svgr-template.js
@@ -1,13 +1,14 @@
 /**
  * svgr の出力を整形するテンプレート
  * .tsx として出力するためのもの
- * props に type を与えれば入力補完が得られる
  */
 exports.default = function template(code, config, state) {
   return `
 import * as React from 'react';
 
-const ${state.componentName} = (props: any) => ${code}
+const ${
+    state.componentName
+  } = (props: React.SVGAttributes<SVGElement>) => ${code}
 
 export default ${state.componentName};
 `.trimLeft();
