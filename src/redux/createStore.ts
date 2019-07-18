decf86a253bc0451197333941279752f3e538181
teramotodaiki <i101323@gmail.com>
2019-07-18 10:52:30 +0900

    fix: remove redux/asset

--- src/redux/createStore.ts
+++ src/redux/createStore.ts
@@ -2,20 +2,14 @@ import * as redux from 'redux';
 import { createLogger } from 'redux-logger';
 import { combineEpics, createEpicMiddleware } from 'redux-observable';
 import { Action } from 'typescript-fsa';
-import { asset, canvas, mode, palette, StoreState } from '.';
+import { canvas, mode, palette, StoreState } from '.';
 
-export const rootEpic = combineEpics(
-  palette.epics,
-  canvas.epics,
-  mode.epics,
-  asset.epics
-);
+export const rootEpic = combineEpics(palette.epics, canvas.epics, mode.epics);
 
 export const rootReducer = redux.combineReducers<StoreState>({
   palette: palette.default,
   canvas: canvas.default,
-  mode: mode.default,
-  asset: asset.default
+  mode: mode.default
 });
 
 const epicMiddleware = createEpicMiddleware<Action<any>, any, StoreState>();
--- src/redux/index.ts
+++ src/redux/index.ts
@@ -1,17 +1,15 @@
 import { Epic as BaseEpic } from 'redux-observable';
 import { Action } from 'typescript-fsa';
-import * as asset from './asset';
 import * as canvas from './canvas';
 import * as mode from './mode';
 import * as palette from './palette';
 
-export { palette, canvas, mode, asset };
+export { palette, canvas, mode };
 
 export type StoreState = {
   canvas: canvas.State;
   palette: palette.State;
   mode: mode.State;
-  asset: asset.State;
 };
 
 export type Epic = BaseEpic<Action<any>, Action<any>, StoreState, void>;
