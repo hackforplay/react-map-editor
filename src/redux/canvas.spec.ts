54cc62f3a6c16ee832d94edc2c6545f282435308
teramotodaiki <i101323@gmail.com>
2019-07-18 11:59:44 +0900

    fix: drawing with immer

--- src/redux/canvas.spec.ts
+++ src/redux/canvas.spec.ts
@@ -1,3 +1,4 @@
+import { SceneMap } from '@hackforplay/next';
 import test from 'ava';
 import Cursor from '../utils/cursor';
 import { draw } from './canvas';
@@ -16,12 +17,19 @@ test('draw canvas', t => {
   };
 
   const cursor = new Cursor(1, 0, 'pen', [[sky], [sky]], 1);
-  const origin = [[[0, 0, 0], [0, 0, 0], [0, 0, 0]]];
-  const expect = [[[0, 1, 0], [0, 1, 0], [0, 0, 0]]];
+  const origin = createSceneMap([[[0, 0, 0], [0, 0, 0], [0, 0, 0]]]);
+  const expect = createSceneMap([[[0, 1, 0], [0, 1, 0], [0, 0, 0]]]);
 
   const result = draw(origin, cursor);
 
   t.not(origin, result, 'draw関数は Immutable でなければならない');
-  t.is(result.length, expect.length);
+  t.is(result.tables.length, expect.tables.length);
   t.deepEqual(result, expect);
 });
+
+function createSceneMap(tables: number[][][]): SceneMap {
+  return {
+    tables,
+    squares: []
+  };
+}
--- src/redux/canvas.ts
+++ src/redux/canvas.ts
@@ -1,13 +1,9 @@
 import { Placement, SceneMap } from '@hackforplay/next';
-import { cloneDeep, flattenDepth } from 'lodash';
+import produce from 'immer';
 import { combineEpics } from 'redux-observable';
-import { filter, map } from 'rxjs/operators';
 import actionCreatorFactory from 'typescript-fsa';
 import { reducerWithInitialState } from 'typescript-fsa-reducers/dist';
-import { Epic } from '.';
 import Cursor from '../utils/cursor';
-import { ITile } from './palette';
-import { ofAction } from './typescript-fsa-redux-observable';
 
 const actionCreator = actionCreatorFactory('react-map-editor/canvas');
 export const actions = {
@@ -19,100 +15,62 @@ export const actions = {
 export interface State extends SceneMap {}
 const initialState: State = init();
 
-export default reducerWithInitialState(initialState)
-  .case(actions.initMap, (state, payload) => payload)
-  .case(actions.set, (state, payload) => payload);
+export const draw = produce(({ tables, squares }: SceneMap, cursor: Cursor) => {
+  if (!cursor.nib || !Array.isArray(tables)) return;
 
-const penEpic: Epic = (action$, state$) =>
-  action$.pipe(
-    ofAction(actions.draw),
-    filter(action => action.payload.mode === 'pen'),
-    map(action => {
-      let { tables, squares } = state$.value.canvas;
-      const { nib } = action.payload;
-      if (!nib) throw new Error('Nib is null');
-      for (const item of flattenDepth<ITile>(nib, 2)) {
-        if (squares.every(s => s.index !== item.index)) {
-          squares = squares.concat({
-            index: item.index,
-            placement: item.placement,
+  if (cursor.mode === 'pen') {
+    for (const [y, row] of cursor.nib.entries()) {
+      for (const [x, tile] of row.entries()) {
+        // drawing
+        const layer = autoLayer(tile.placement);
+        const X = cursor.x + x;
+        const Y = cursor.y + y;
+
+        const table = tables && tables[layer];
+        const tableRow = table && table[Y];
+        if (0 <= X && X < tableRow.length) {
+          tableRow[X] = tile.index;
+        }
+        // add tile info
+        if (squares.every(s => s.index !== tile.index)) {
+          squares.push({
+            index: tile.index,
+            placement: tile.placement,
             tile: {
               size: [32, 32],
               image: {
                 type: 'url',
-                src: item.src
+                src: tile.src
               },
-              author: item.author
+              author: tile.author
             }
           });
         }
       }
-      return {
-        tables: draw(tables, action.payload),
-        squares
-      };
-    }),
-    map(map => actions.set(map))
-  );
+    }
+  }
 
-const eraserEpic: Epic = (action$, state$) =>
-  action$.pipe(
-    ofAction(actions.draw),
-    filter(action => action.payload.mode === 'eraser'),
-    map(action => {
-      const { tables, squares } = state$.value.canvas;
-      return {
-        tables: draw(tables, action.payload),
-        squares
-      };
-    }),
-    map(map => actions.set(map))
-  );
-
-export const epics = combineEpics(penEpic, eraserEpic);
-
-/**
- * ３次元配列を置き換える.
- * @param origin 元の３次元配列
- * @param cursor 置き換える Cursor
- */
-export function draw(origin: number[][][], cursor: Cursor) {
-  switch (cursor.mode) {
-    case 'pen':
-      if (!cursor.nib) return origin;
-      const result = cloneDeep(origin);
-      cursor.nib.forEach((row, y) =>
-        row.forEach((square, x) => {
-          const layer = autoLayer(square.placement);
-          const X = cursor.x + x;
-          const Y = cursor.y + y;
-          if (
-            layer < result.length &&
-            Y < result[layer].length &&
-            X < result[layer][Y].length
-          ) {
-            result[layer][Y][X] = square.index;
-          }
-        })
-      );
-      return result;
-    case 'eraser':
-      for (const [layer, table] of origin.entries()) {
-        if (table[cursor.y][cursor.x] > -1) {
-          // 空白じゃないマスを見つけた.
-          // しかし, オートレイヤー状態では一番下のレイヤーは消せない
-          if (layer !== origin.length - 1) {
-            const result = cloneDeep(origin);
-            result[layer][cursor.y][cursor.x] = -888; // ４桁にしたい
-            return result;
-          }
+  if (cursor.mode === 'eraser') {
+    for (const [layer, table] of tables.entries()) {
+      const tableRow = table && table[cursor.y];
+      const index = tableRow && tableRow[cursor.x];
+      if (index > -1) {
+        // 空白じゃないマスを見つけた.
+        // しかし, オートレイヤー状態では一番下のレイヤーは消せない
+        if (layer !== tables.length - 1) {
+          tableRow[cursor.x] = -888; // ４桁にしたい
         }
       }
-      return origin; // 見つからなかった
-    case 'nope':
-      return origin;
+    }
   }
-}
+});
+
+export default reducerWithInitialState(initialState)
+  .case(actions.initMap, (state, payload) => payload)
+  .case(actions.set, (state, payload) => payload)
+  .case(actions.draw, draw);
+
+export const epics = combineEpics();
 
 /**
  * マップの初期値
