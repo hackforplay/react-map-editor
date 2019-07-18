1b50a50f8bf3d9c77f5469759d24ddf95aeefe57
teramotodaiki <i101323@gmail.com>
2019-07-18 14:31:57 +0900

    fix: use lodash-es instead of lodash

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,5 @@
 import { Scene } from '@hackforplay/next';
-import { cloneDeep } from 'lodash';
+import { cloneDeep } from 'lodash-es';
 import * as React from 'react';
 import { Provider } from 'react-redux';
 import { Root, RootProps } from './components/Root';
