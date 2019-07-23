18f76e7763cd8ec996d8a8d8d256501c124d0ba9
teramotodaiki <i101323@gmail.com>
2019-07-23 10:50:48 +0900

    fix: collider settings

--- src/components/SettingView.tsx
+++ src/components/SettingView.tsx
@@ -1,11 +1,32 @@
 import * as csstips from 'csstips/lib';
 import * as React from 'react';
+import { useDispatch } from 'react-redux';
 import { style } from 'typestyle/lib';
+import { useTypedSelector } from '../hooks/useTypedSelector';
+import { actions } from '../redux/canvas';
 
 const container = style(csstips.flex1, {
   minWidth: 120
 });
 
 export function SettingView() {
-  return <div className={container} />;
+  const dispatch = useDispatch();
+  const debug = useTypedSelector(state => state.canvas.debug);
+
+  const toggleDebug = React.useCallback(() => {
+    dispatch(actions.initMap({ debug: !debug }));
+  }, [debug]);
+
+  return (
+    <div className={container}>
+      <label htmlFor="debug">collider: </label>
+      <input
+        type="checkbox"
+        name="debug"
+        id="debug"
+        checked={debug}
+        onChange={toggleDebug}
+      />
+    </div>
+  );
 }
